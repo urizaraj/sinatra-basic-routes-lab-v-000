49e3b065d2960a811499ade5747faeb7bdf11ae6
Alexander Urizar <urizaraj@gmail.com>
2018-02-27 22:59:49 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get 'name' do
+  get '/name' do
     'My name is Alex'
   end
 
-  get 'hometown' do
+  get '/hometown' do
     'My hometown is Monroe'
   end
 
-  get 'favorite-song' do
+  get '/favorite-song' do
     'My favorite song is Raid'
   end
 end
